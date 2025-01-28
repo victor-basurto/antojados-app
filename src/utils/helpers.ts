f29c8f1ca9ac267358562228396255395bd3fe92
Victor Basurto <my.mvplace@gmail.com>
2025-01-28 12:02:02 -0800

    updated helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,16 @@
 /**
+ * checks if number is type number
+ * @param {number} num 
+ * @returns bool
+ */
+export const isNumber = (num: string | number) => (typeof(num) === 'number' || typeof(num) === "string" && num.trim() !== '') && !isNaN(num as number);
+/**
  *
  * @param {number} currency
  * @returns
  */
-export const formatCurrency = (currency: number) => currency.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+export const formatCurrency = (currency: number) => {
+	if (!isNumber(currency)) return;
+  return currency.toLocaleString('en-US', {style: 'currency', currency: 'USD'})
+};
+
