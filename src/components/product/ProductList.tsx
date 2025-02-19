b0bd94b54a45900d63e806d8d60a00cdc16ffe3a
Victor Basurto <my.mvplace@gmail.com>
2025-02-19 01:02:30 -0800

    created two icons to switch between card component or the table component

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import { CiGrid42, CiViewTable } from "react-icons/ci";
 import { fetchProducts } from '@/utils/fetchData';
 import type {
   IProduct,
@@ -9,6 +10,7 @@ import type {
 import ProductTable from './ProductTable';
 import ProductCardList from './ProductCardList';
 const ProductList: React.FC = () => {
+  const [view, setView] = useState<'table' | 'grid'>('grid');
   const [productData, setProductData] = useState({
     data: [] as IProduct[],
     message: '',
@@ -43,12 +45,25 @@ const ProductList: React.FC = () => {
   }, []);
   return (
     <div>
-      <ProductTable
+      <div className="w-100 flex items-center justify-end">
+        <div className="icons flex w-[35px] justify-between items-center my-4">
+          {/* if clicked in table -> display <ProductTable /> else <ProductCardList /> */}
+          <button onClick={() => setView('table')} aria-label="table view">
+            <CiViewTable className={view === 'table' ? 'active' : ''} size={24} />
+          </button>
+          <button onClick={() => setView('grid')} aria-label="grid view">
+            <CiGrid42 className={view === 'grid' ? 'active' : ''} size={24} />
+          </button>
+        </div>
+      </div>
+      { view === 'table' ? (
+        <ProductTable
         classNames="dark"
         data={productData}
         tableHeaders={tableHeaders}
-      />
-      <ProductCardList products={productData} />
+      />) : (
+        <ProductCardList products={productData} />
+      )}
     </div>
   );
 };
